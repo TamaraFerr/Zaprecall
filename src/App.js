598609c4b082eb6e907cbdbc1c69eb3721b0bd36
TamaraFerr <tamarablacksmith@hotmail.com>
2023-02-28 01:12:08 -0300

    inicio do projeto ZapRecall

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Flashcards from "./Flashcards";
+import "./styles/reset.css"
+import "./styles/style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="zap-body">
+        <div className="top">
+          <img src="./assets/logo.png" alt="zaprecall-logo" className="zap-logo"/>
+          <h1>ZapRecall</h1>
+        </div>       
+
+        <Flashcards />
+
+        <div className="zap-footer">
+          <h2>0/4 CONCLUÍDOS</h2>
+        </div>
+      </div>    
+    </>
+  )
 }
 
 export default App;
---
+++ src/Flashcards.js
@@ -0,0 +1,22 @@
+export default function Flashcards() {
+    return (
+        <div className="questions-container">
+            <div className="zap-questions">
+                <p>dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start"/>
+            </div>
+            <div className="zap-questions">
+                <p>dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start"/>
+            </div>
+            <div className="zap-questions">
+                <p>dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start"/>
+            </div>
+            <div className="zap-questions">
+                <p>dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start"/>
+            </div>
+        </div>
+    )
+}
