d8cc87abbc15fe7946165c49c6548e6583ae7892
TamaraFerr <tamarablacksmith@hotmail.com>
2023-03-01 00:15:05 -0300

    finalização das telas para o layout

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -1,6 +1,42 @@
 export default function Flashcards() {
     return (
         <div className="questions-container">
+            <div className="zap-question-front" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_virar.png" className="zap-vira" alt="zap-vira" data-test="play-btn"/>
+            </div>
+            <div className="zap-question-answer" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasdasdasdasdasdasdasdasdsdasdasdasdasd</p>
+                <div className="button">
+                    <button className="botao1">Não lembrei</button>
+                    <button className="botao2">Quase não lembrei</button>
+                    <button className="botao3">Zap!</button>
+                </div>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
+            <div className="zap-questions" data-test="flashcard">
+                <p data-test="flashcard-text">dasdasdasd</p>
+                <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
+            </div>
             <div className="zap-questions" data-test="flashcard">
                 <p data-test="flashcard-text">dasdasdasd</p>
                 <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn"/>
