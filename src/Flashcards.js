676c0528a646815626aaef231278f386d1dfbb53
Tamara Ferreira <tamarablacksmith@hotmail.com>
2023-03-02 08:44:26 -0300

    adicionando data-test

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -1,16 +1,18 @@
+import styled from "styled-components"
+
 export default function Flashcards() {
     return (
         <div className="questions-container">
             <div className="zap-question-front" data-test="flashcard">
                 <p data-test="flashcard-text">dasdasdasd</p>
-                <img src="./assets/seta_virar.png" className="zap-vira" alt="zap-vira" data-test="play-btn"/>
+                <img src="./assets/seta_virar.png" className="zap-vira" alt="zap-vira" data-test="turn-btn"/>
             </div>
             <div className="zap-question-answer" data-test="flashcard">
                 <p data-test="flashcard-text">dasdasdasdasdasdasdasdasdasdasdsdasdasdasdasd</p>
                 <div className="button">
-                    <button className="botao1">Não lembrei</button>
-                    <button className="botao2">Quase não lembrei</button>
-                    <button className="botao3">Zap!</button>
+                    <button className="botao1" data-test="no-btn" >Não lembrei</button>
+                    <button className="botao2" data-test="partial-btn">Quase não lembrei</button>
+                    <button className="botao3" data-test="zap-btn">Zap!</button>
                 </div>
             </div>
             <div className="zap-questions" data-test="flashcard">
