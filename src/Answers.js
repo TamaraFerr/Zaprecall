4a3e3fd8006a42a5967f5113e88f833723ed6023
TamaraFerr <tamarablacksmith@hotmail.com>
2023-03-06 00:58:28 -0300

    finalizando renderização da tela

--- src/Answers.js
+++ src/Answers.js
@@ -1,16 +1,28 @@
 
 
-export default function Answers(props) {
+export default function Answers(props) {  
+    const wrong = ["erro", "./assets/icone_erro.png", "no-icon"]
+    const almost = ["quase", "./assets/icone_quase.png", "partial-icon"]
+    const zap = ["certo", "./assets/icone_certo.png", "zap-icon"]
 
+    function handleClick(classe, icon, teste) {
+        props.clicked(false)
+        props.setConfig({
+            classe: classe, 
+            icon: icon,
+            teste: teste
+        })
 
+        console.log(classe)
+    }
 
     return (
         <div className="zap-question-answer" data-test="flashcard">
             <p data-test="flashcard-text">{props.answer}</p>
             <div className="button">
-                <button className="botao1" data-test="no-btn" >Não lembrei</button>
-                <button className="botao2" data-test="partial-btn">Quase não lembrei</button>
-                <button className="botao3" data-test="zap-btn">Zap!</button>
+                <button className="botao1" data-test="no-btn" onClick={e => {handleClick(...wrong)}} >Não lembrei</button>
+                <button className="botao2" data-test="partial-btn" onClick={e => {handleClick(...almost)}}>Quase não lembrei</button>
+                <button className="botao3" data-test="zap-btn" onClick={e => {handleClick(...zap)}}>Zap!</button>
             </div>
         </div>
     )
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,8 +1,14 @@
 import { useState } from "react"
 import Questions from "./Questions"
 
+
 export default function Flashcard(props) {
     const [clicked, setClicked] = useState(false)
+
+    const [config, setConfig] = useState({
+        classe:"", 
+        icon: "./assets/seta_play.png" , 
+        teste: "play-btn"})
 
     function handleClick() {
         setClicked(true)
@@ -10,10 +16,10 @@ export default function Flashcard(props) {
 
     return (
         <li>
-            {clicked ?  <Questions question={props.question} answer={props.answer}/>  :
-                <div className="zap-questions" data-test="flashcard">
+            {clicked ?  <Questions question={props.question} answer={props.answer} clicked={setClicked} index={props.index} setConfig={setConfig}/>  :
+                <div className={`zap-questions ${config.classe}`} data-test="flashcard">
                     <p data-test="flashcard-text">{`Pergunta ${props.index + 1}`}</p>
-                    <img src="./assets/seta_play.png" className="zap-start" alt="zap-start" data-test="play-btn" onClick={handleClick}/>
+                    <img src={config.icon} className="zap-start" alt="zap-start" data-test={config.teste} onClick={handleClick}/>
                 </div>}
         </li>
     )
